60981c2c67702782018cc925a012fafc069b5f5d
AhlimanMukhtarli <ehliman270@gmail.com>
2023-08-03 14:04:54 +0400

    else and if

---
+++ Lesson-3/Program (2).cs
@@ -0,0 +1,34 @@
+﻿namespace TicketPriceCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your age:");
+            byte age = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("Please enter the day of the week:");
+            string dayoftheweek = Console.ReadLine()?? "";
+            int ticketPrice = 0;
+
+            if (age > 0 && age <= 12)
+            {
+                ticketPrice = 5;
+            }
+            else if (age >= 13 && age <= 64)
+            {
+                ticketPrice = 10;
+            }
+            else if (age >= 65)
+            {
+                ticketPrice = 7;
+            }
+
+            if (dayoftheweek == "wednesday")
+            {
+                ticketPrice -= 2;
+            }
+                 Console.WriteLine("Your ticket price is $"+ ticketPrice);
+
+        }
+    }
+}
---
+++ Lesson-3/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace GradeApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your grade");
+            int score = Convert.ToInt32(Console.ReadLine());
+
+            if (score >= 90 && score <= 100)
+            {
+                Console.WriteLine("Your grade is A");
+            }
+            else if (score >= 80 && score <= 89)
+            {
+                Console.WriteLine("Your grade is B");
+            }
+            else if (score >= 70 && score <= 79)
+            {
+                Console.WriteLine("Your grade is C");
+            }
+            else if (score >= 60 && score <= 69)
+            {
+                Console.WriteLine("Your grade is D");
+            }
+            else if (score >= 0 && score <= 59)
+            {
+                Console.WriteLine("Your grade is E");
+            }
+            else Console.WriteLine("Your grade is not defined");
+        }
+    }
+}
